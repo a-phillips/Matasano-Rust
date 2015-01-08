76756ac553f4b3116c9ebc72844f15782fe553de
Andrew Phillips <arphillips3@me.com>
2015-01-08 00:21:21 -0500

    Challenge 3 improved

--- src/bin/S1C3.rs
+++ src/bin/S1C3.rs
@@ -1,71 +1,44 @@
-//should probably create a module to store this functionality...
-
 extern crate serialize;
 
 use serialize::hex::FromHex;
-use std::num::Int;
+use std::io::BufferedReader;
+use std::io::File;
 
-fn hex_to_bin_vec(hex_str:&str) -> Vec<String> {
-	let mut bin_vec = hex_str.from_hex()
-							.unwrap()
-							.iter()
-							.map(|x| format!("{:b}", *x))
-							.collect::<Vec<String>>();
-	//pad with 0s
-	pad_bin_with_0s(bin_vec)
+fn get_words(fp: &str) -> Vec<String> {
+	let path = Path::new(fp);
+	let mut file = BufferedReader::new(File::open(&path));
+	println!("Compiling word list...");
+	let word_vec:Vec<String> = file.lines().map(|x| x.unwrap()).collect();
+	println!("Word list complete!");
+	word_vec
 }
 
-fn pad_bin_with_0s(input_bin_vec:Vec<String>) -> Vec<String> {
-	let mut bin_vec = input_bin_vec;
-	for i in range(0u, bin_vec.len()) {
-		let mut bin_8 = String::new();
-		for _ in range(0u, 8-bin_vec[i].len()) {
-			//println!("{}", j);
-			bin_8.push('0');
+fn decode(hex_input:&str, words_fp:&str) {
+	let bytes = hex_input.from_hex().unwrap();
+	let word_vec = get_words(words_fp);
+	//brute force through keys
+	for key in range(0u8, 255) {
+		let decoded_str = String::from_utf8(bytes.iter().map( |byte| {*byte^key} ).collect());
+		let valid_str = match decoded_str {
+			Ok(v) => v,
+			Err(_) => "ignore".to_string(), //Breaks at 128, don't know why
+		};
+		if valid_str != "ignore" {
+			'outer: for word in valid_str.words() { //need to break this loop if word found, lebeled "outer" for break
+				for check_word in word_vec.iter() {
+					if word.as_slice() == check_word.as_slice().slice_to(check_word.len() - 1) { //check_word has newline at end
+						println!("{}", valid_str); //print a result that has some word that exists in word_vec
+						break 'outer;
+					}
+				}
+			}
 		}
-		bin_8.push_str(bin_vec[i].as_slice());
-		bin_vec[i] = bin_8;
-	}
-	bin_vec
-}
-
-fn xor(str1:&str, str2:&str) -> String {
-	let mut result = String::new();
-	for i in range(0u, str1.len()) {
-		if str1.slice(i, i+1) == str2.slice(i, i+1) { result.push('0'); }
-		else { result.push('1'); }
-	}
-	result
-}
-
-fn bin_to_u8(bin:&str) -> u8 {
-	let mut result = 0u;
-	let mut raise_to = bin.len()-1;
-	for num in bin.chars() {
-		result += (2u.pow(raise_to))*num.to_digit(10).unwrap();
-		raise_to -= 1;
-	}
-	result as u8
-}
-
-fn decode(hex_input:&str) {
-	let bin = hex_to_bin_vec(hex_input.as_slice());
-	let decoders = pad_bin_with_0s(range(0u64, 256).map(|x| format!("{:b}", x)).collect::<Vec<String>>());
-	//println!("{}", bin);
-	//println!("{}", decoders);
-	//brute force through the decoders
-	for key in decoders.iter() {
-		let mut bin_xor = Vec::new();
-		for i in range(0u, bin.len()) {
-			bin_xor.push(bin_to_u8(xor(bin[i].as_slice(), key.as_slice()).as_slice()));
-		}
-		println!("{}, {}", bin_to_u8(key.as_slice()), String::from_utf8(bin_xor).unwrap());
 	}
 }
 
 fn main() {
 	let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-	decode(input.as_slice());
+	let words_fp = "src/words.txt"; //google's 10,000 words list
+	decode(input.as_slice(), words_fp.as_slice());
 	//"X" - Cooking MC's like a pound of bacon
-	//process breaks at 128th iteration, need to investigate...
 }
