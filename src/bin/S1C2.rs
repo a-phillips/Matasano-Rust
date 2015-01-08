9fcb315ad64d8e2b41c01c6f92ac765e37e2ef82
Andrew Phillips <arphillips3@me.com>
2015-01-07 22:10:12 -0500

    significant revision to S1C2 - learned about xor operator

--- src/bin/S1C2.rs
+++ src/bin/S1C2.rs
@@ -1,56 +1,18 @@
 //why is converting to binary and xor-ing so difficult?
+//turns out, it's not! thanks sean!
 
 extern crate serialize;
 
 use serialize::hex::{FromHex, ToHex};
-use std::num::Int;
-
-fn hex_to_bin_vec(hex_str:&str) -> Vec<String> {
-	let mut bin_vec = hex_str.from_hex()
-							.unwrap()
-							.iter()
-							.map(|x| format!("{:b}", *x))
-							.collect::<Vec<String>>();
-	//pad with 0s
-	for i in range(0u, bin_vec.len()) {
-		let mut bin_8 = String::new();
-		for _ in range(0u, 8-bin_vec[i].len()) {
-			//println!("{}", j);
-			bin_8.push('0');
-		}
-		bin_8.push_str(bin_vec[i].as_slice());
-		bin_vec[i] = bin_8;
-	}
-	bin_vec
-}
-
-fn xor(str1:&str, str2:&str) -> String {
-	let mut result = String::new();
-	for i in range(0u, str1.len()) {
-		if str1.slice(i, i+1) == str2.slice(i, i+1) { result.push('0'); }
-		else { result.push('1'); }
-	}
-	result
-}
-
-fn bin_to_u8(bin:&str) -> u8 {
-	let mut result = 0u;
-	let mut raise_to = bin.len()-1;
-	for num in bin.chars() {
-		result += (2u.pow(raise_to))*num.to_digit(10).unwrap();
-		raise_to -= 1;
-	}
-	result as u8
-}
 
 fn fixed_xor_hex(buff1: &str, buff2: &str) -> String {
-	let bin1 = hex_to_bin_vec(buff1);
-	let bin2 = hex_to_bin_vec(buff2);
-	let mut bin_xor = Vec::new();
-	for i in range(0u, bin1.len()) {
-		bin_xor.push(bin_to_u8(xor(bin1[i].as_slice(), bin2[i].as_slice()).as_slice()));
-	}
-	bin_xor.to_hex()
+	let bytes1 = buff1.from_hex().unwrap();
+	let bytes2 = buff2.from_hex().unwrap();
+	let bytes_xor:Vec<u8> = bytes1.iter()
+							.zip(bytes2.iter())
+							.map(| (b1, b2) | {*b1^*b2} )
+							.collect();
+	bytes_xor.to_hex()
 }
 
 fn main() {
